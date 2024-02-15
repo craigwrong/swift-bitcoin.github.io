040d7e457333b99712a18be67e7db79bbaf9057d
craigwrong <95532896+craigwrong@users.noreply.github.com>
2024-02-15 18:03:46 +0000

    Correction to home page title and description.

--- src/content/page/pageHome.swift
+++ src/content/page/pageHome.swift
@@ -2,9 +2,9 @@ import SwiftySites
 
 let pageHome = Page("/", "Home") { #"""
 
-# Welcome to Swift Bitcoin
+# Swift Bitcoin
 
-_Swift Bitcoin_ is a Pure-Swift Bitcoin client implementation with full node capabilities.
+Pure-Swift Bitcoin client implementation with full node capabilities.
 
 Find out more about it [here](/info).
 
